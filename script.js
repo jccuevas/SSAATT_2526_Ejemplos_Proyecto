6fb28ecfc4b6eeef8a003cd6a84601813c8db6a0
usuario <usuario@DESKTOP-73UH5AV>
2026-03-16 16:24:53 +0100

    Fase 2 - Sesión 2 - ejemplo cambio de pestañas

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 
-let section = document.getElementById("acercade");
-section.style.display="none";
-
 
 function showError(event, error){
     let div = document.getElementById("errorBanner");
@@ -10,4 +7,30 @@ function showError(event, error){
         div.classList.add("visible");
     }
 
+}
+
+function showSection(sectionId){
+
+    console.log("Mostrar sección "+sectionId);
+    //let sections = document.querySelectorAll("[id|=sec-]");
+    let sections = document.getElementsByTagName("section");
+    if(sections!=null){
+        for(let s of sections){
+            s.classList.add("oculta");
+        }
+
+        let section = document.getElementById("sec-"+sectionId);
+        section.classList.remove("oculta");
+
+        let links = document.querySelectorAll("[id|=nav]");
+        console.dir(links);
+        if(links!=null){
+            for(let link of links){
+                link.classList.remove("active");
+            }
+
+            let link = document.getElementById("nav-"+sectionId);
+            link.classList.add("active");
+        }
+    }
 }
